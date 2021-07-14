b6d0dfef35ab042b1a8381d278793a31faf17b13
Dmitry Diakov <d.diakov@list.ru>
2021-07-15 00:12:43 +0500

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
+import Person from './person';
+
+export default class Team {
+  constructor(...args) {
+    this.team = [];
+    args.forEach((e) => {
+      this.team.push(new Person(e));
+    });
+  }
+
+  * [Symbol.iterator]() {
+    for (const a of this.team) {
+      yield a;
+    }
+  }
+}
+
+const team1 = new Team('Bowman', 'Swordsman', 'Demon');
+
+for (const value of team1) {
+  console.log(value);
+}
---
+++ src/js/person.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+export default class Person {
+  constructor(type) {
+    this.name = type;
+    this.type = type;
+    this.health = 50;
+    this.level = 1;
+    this.attack = 40;
+    this.defense = 10;
+  }
+}
